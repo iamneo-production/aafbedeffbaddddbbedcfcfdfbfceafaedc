1f93ea179fbc01c392602e43843a1d683b806028
Iamneo <techadmin@iamneo.ai>
2023-08-05 13:39:04 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -4,9 +4,46 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.
+import org.springframework.web.bind.annotation.*;
+import com.examly.springapp.entity.Task;
+import com.examly.springapp.service.TaskService;
 
 @RestController
 public class TaskController {
+    @Autowired
+	private TaskService taskService;
+	
+	@PostMapping("/saveTask")
+	public Task saveTask(@RequestBody Task task) {
+		return taskService.saveTask(task);
+	}
+
+    @GetMapping("/changeStatus")
+	public Task changeStatus(@RequestParam String id) {
+		return taskService.changeStatus(id);
+	}
+	
+    @DeleteMapping("/deleteTask/{id}")
+	public void deleteTask(@RequestParam String id) {
+		taskService.deleteTask(id);
+	}
     
+	@GetMapping("/getTasks")
+	public List<Task> getAllTasks() {
+		return taskService.getAllTasks();
+	}
+	
+	@GetMapping("/getTaskById/{id}")
+	public Task getTaskById(@RequestParam String id) {
+		return taskService.getTaskById(id);
+	}
+	
+	@GetMapping("/getTaskByName/{name}")
+	public Task getTaskByTaskHolderName(@RequestParam String taskHolderName) {
+		return taskService.getTaskByTaskHolderName(taskHolderName);
+	}
+	
+	
+	
+	
 }
